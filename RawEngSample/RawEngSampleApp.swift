29c8bf861569174166f3655567abdc0c14e4ab7d
Aryan Sharma <agsharma98@gmail.com>
2024-10-24 02:17:18 -0700

    Initial Commit

---
+++ RawEngSample/RawEngSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawEngSampleApp.swift
+//  RawEngSample
+//
+//  Created by Aryan Sharma on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RawEngSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
